5b07fb74bc87986ce259b125c906dd15512dba59
Chet Wright <chet.wright@centre.edu>
2025-02-18 17:18:57 -1200

    working

--- src/main/java/Payroll/A.java
+++ src/main/java/Payroll/A.java
@@ -1,5 +1,5 @@
 //this is a program, there are no other comments necessary
 package Payroll;
 import java.util.List;
-abstract class A {	String n0;float h0;float h1;public A(String n0, float h0){super();this.n0 = n0;this.h0 = h0;this.h1 = 0;}public void s0000000(float h1) {this.h1 = h1;}public String g0() {return n0;}public abstract float c00(); public float g1(){return h1;}public static void s00000001(A A0, float h1){A0.s0000000(h1);}public static void p0(List<A> AS) {for (A a : AS){System.out.println(a.g0() + " is to be payed " + a.c00());}}};
+abstract class A {String n0;float h0;float h1;public A(String n0,float h0){super();this.n0=n0;this.h0=h0;this.h1=0;}public void s0000000(float h1){this.h1=h1;}public String g0() {return n0;}public abstract float c00(); public float g1(){return h1;}public static void s00000001(A A0,float h1){A0.s0000000(h1);}public static void p0(List<A> AS) {for (A a : AS){System.out.println(a.g0() + " is to be payed " + a.c00());}}};
 
